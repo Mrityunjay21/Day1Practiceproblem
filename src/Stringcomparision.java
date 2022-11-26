2af01ae65d1a36505cdb9b461077be1ccfaf78ce
Mrityunjay21 <mrityunjaynayak2021@gmail.com>
2022-11-26 21:53:22 +0530

    initial commit

---
+++ src/Stringcomparision.java
@@ -0,0 +1,21 @@
+
+public class Stringcomparision {
+
+	public static void main(String[] args) {
+		String str1="Hello";
+		String str2="Hello";
+		String str3="rahul";
+		System.out.println(str1.equals(str3));
+		System.out.println(str1.equals(str2));
+}
+}
+
+// To find the sum of command-line arguments
+
+    public static void main1(String args[]){
+    	
+    	int n1 =225,n2=115;
+    	sum = n1 + n2;
+    	System.out.println("The sum of number is:" + sum);
+    }
+    
